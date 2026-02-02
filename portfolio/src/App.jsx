4f166894db4c697d7844b706ae987d2c912dd830
Akhi Alam <akhi42218@gmail.com>
2026-02-02 10:24:58 +0400

    portfolio ready

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./component/Navbar";
 import Home from "./component/Home";
 import About from "./component/About";
 import Experience from "./component/Experience";
-import Projects from "./component/project";
+import Projects from "./component/Project";
 import Contact from "./component/Contact";
 
 function App() {
